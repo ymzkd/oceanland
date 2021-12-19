3ef53470db0fc568545d394512426ea6da548603
ymzkd <ymz.ptr@gmail.com>
2021-12-19 21:24:59 +0900

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -6,35 +6,74 @@
       dark
     >
       <div class="d-flex align-center">
-        <v-img
+        <!-- <v-img
           alt="Vuetify Logo"
           class="shrink mr-2"
           contain
           src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
           transition="scale-transition"
           width="40"
-        />
+        /> -->
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <h2> Culculation Sheets </h2>
       </div>
 
       <v-spacer></v-spacer>
+      <v-dialog
+        v-model="dialog"
+        width="500"
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <!-- <v-btn
+            color="darkgrey lighten-2"
+            dark
+            v-bind="attrs"
+            v-on="on"
+          > -->
+          <v-icon 
+            color="darkgrey lighten-2"
+            dark
+            v-bind="attrs"
+            v-on="on"
+            large
+          >
+            mdi-information-outline
+          </v-icon>
+          <!-- </v-btn> -->
+        </template>
 
-      <v-btn
+        <v-card>
+          <v-card-title class="text-h5 grey lighten-2">
+            本計算シートについて
+          </v-card-title>
+
+          <v-card-text>
+            標高は国土地理院にて提供されるDEMデータから標高を求めるAPI「<a href="https://maps.gsi.go.jp/development/elevation_s.html">サーバサイドで経緯度から標高を求めるプログラム</a>」を用いている。
+            海率は海岸線データを平成十四年国土交通省告示第九号に定める<a href="https://www.gsi.go.jp/LAW/heimencho.html">平面直角座標系</a>に基づく投影方法にて投影し、算定したものである。なお、採用する平面座標系は告示別表の地域別ではなく簡単のため計算地点に最も近い座標系を採用する。
+          </v-card-text>
+
+          <v-divider></v-divider>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              color="primary"
+              text
+              @click="dialog = false"
+            >
+              OK
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+      <!-- <v-btn
         href="https://github.com/vuetifyjs/vuetify/releases/latest"
         target="_blank"
         text
       >
         <span class="mr-2">Latest Release</span>
         <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      </v-btn> -->
     </v-app-bar>
 
     <v-main>
@@ -54,7 +93,7 @@ export default {
   },
 
   data: () => ({
-    //
+    dialog: false,
   }),
 };
 </script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,8 @@
 <template>
   <v-container>
     <v-row class="text-center">
+
+      <!-- Top Description -->
       <v-col class="mb-4">
         <h1 class="display-2 font-weight-bold mb-3">
           海率計算
@@ -10,63 +12,101 @@
           積雪荷重計算のための任意地点から任意半径内の円の面積に対する海の面積の比率(海率)を計算する。
         </p>
       </v-col>
-
-      <v-col cols="12">
-        <v-img
-          :src="plot_src"
-          class="my-3"
-          contain
-        />
-          <!-- height="200" -->
-      </v-col>
     </v-row>
 
-    <v-form>
-      <v-container>
-        <v-row>
-          <v-col
-            cols="12"
-            sm="6"
-            md="3"
-          >
-            <v-text-field
-              label="Longitude"
-              placeholder="Longitude"
-              v-model="longitude"
-            ></v-text-field>
-          </v-col>
-          <p>経度</p>
-          <v-col
-            cols="12"
-            sm="6"
-            md="3"
-          >
-              <!-- label="Latitude" -->
-            <v-text-field
-              placeholder="Latitude"
-              v-model="latitude"
-            ></v-text-field>
-          </v-col>
-          <p>半径</p>
-          <v-col
-            cols="12"
-            sm="6"
-            md="3"
-          >
-              <!-- label="Latitude" -->
-            <v-text-field
-              placeholder="Radious"
-              v-model="radious"
-            ></v-text-field>
-          </v-col>
-          <v-btn
-            elevation="2"
-            @click="calculate"
-          > calculate </v-btn>
-        </v-row>
-      </v-container>
-    </v-form>
-    <p> 海抜: {{height}} m, 海率: {{rate.toFixed(5)}} </p>
+    <v-row>
+      <!-- Graph Zone -->
+      <v-col cols="12" sm="12" md="6" lg="6"
+        class="mapplot pa-3"
+      >
+          <!-- class="mapplot"  -->
+
+        <v-img
+          max-height="450px"
+          v-if="loaded"
+          :src="plot_src"
+          contain
+        />
+
+        <v-progress-circular
+          v-else
+          indeterminate>
+        </v-progress-circular>
+          <!-- height="200" -->
+      </v-col>
+      <!-- Input and Result Zone -->
+      <v-col cols="12" sm="12" md="6" lg="6">
+        <v-form>
+          <v-container>
+            <!-- Inputs -->
+            <v-row>
+              <v-col cols="12" xs="12" sm="6" md="3" lg="3">
+                <v-text-field
+                  type="number"
+                  label="緯度"
+                  placeholder="Longitude"
+                  suffix="°"
+                  v-model="longitude"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" xs="12" sm="6" md="3" lg="3">
+                <v-text-field
+                  type="number"
+                  label="経度"
+                  placeholder="Latitude"
+                  suffix="°"
+                  v-model="latitude"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" xs="12" sm="6" md="3" lg="3">
+                <v-text-field
+                  type="number"
+                  label="半径"
+                  placeholder="Radious"
+                  v-model="radious"
+                  suffix="km"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" xs="12" sm="6" md="3" lg="3">
+                <v-btn
+                  elevation="2"
+                  @click="calculate"
+                > calculate </v-btn>
+              </v-col>
+            </v-row>
+
+            <!-- Results -->
+            <v-row>
+              <v-col
+                cols="12"
+                sm="6"
+              >
+                <v-text-field
+                  :value="height"
+                  label="海抜"
+                  suffix="m"
+                  readonly
+                ></v-text-field>
+              </v-col>
+              <v-col
+                cols="12"
+                sm="6"
+              >
+                <v-text-field
+                  :value="(rate*100).toFixed(5)"
+                  label="海率"
+                  suffix="%"
+                  readonly
+                  autofocus="false"
+                ></v-text-field>
+              </v-col>
+            </v-row> <!-- Results -->
+            
+            
+          </v-container>
+        </v-form>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -76,6 +116,7 @@
     name: 'HelloWorld',
 
     data: () => ({
+      loaded: true,
       longitude: 139.7454329,
       latitude: 35.6585805,
       radious: 40,
@@ -90,6 +131,7 @@
     },
     methods: {
       calculate(){
+        this.loaded = false;
         axios
         .post(this.apiurl + 'pos', {
           lon: String(this.longitude),
@@ -97,6 +139,7 @@
           rad: String(this.radious)
         })
         .then((res) => {
+          this.loaded = true;
           this.height = res.data.elevation
           this.rate = res.data.sea_ratio
           this.plot_src = "data:image/png;base64," + res.data.image_src;
@@ -106,3 +149,11 @@
     }
   }
 </script>
+
+<style scoped>
+.mapplot {
+  border: 1px solid;
+  height: 500px;
+  border-radius: 2px;
+}
+</style>
